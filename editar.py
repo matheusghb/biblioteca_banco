8674f45f29a1cce94e083ffbbcf5b6585c672f50
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2024-04-09 17:32:06 -0300

    Update editar.py

--- editar.py
+++ editar.py
@@ -3,7 +3,7 @@ from conexao import connect
 def editar(mydb, titulo, autor, ano, sta):
  mycursor = mydb.cursor()
  titulo = input ('fale o nome do livro pfv')
-autor, ano, sta = input ('fale os novos atributos do livro')
+autor, ano, sta = input ('fale o autor, o ano e o status do livro ( nessa ordem )')
   sql =  "UPDATE livros SET autor = %s, ano = %s, sta = %s WHERE titulo = %s"
   val = (titulo, autor, ano, sta) 
   mycursor.execute(sql, val)
