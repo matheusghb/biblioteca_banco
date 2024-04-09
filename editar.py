fe6366064b95cd69bfa28331168bbb63372b12b4
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2024-04-09 17:15:49 -0300

    yti editar.py

--- editar.py
+++ editar.py
@@ -1,3 +1,13 @@
 from conexao import connect
 
-def editar(mydb, pesquisa)
+def editar(mydb, titulo, autor, ano, sta):
+ mycursor = mydb.cursor()
+
+  sql =  "INSERT INTO livros (titulo, autor, ano, status) VALUES (%s, %s, %s, %s)"
+  val = (titulo, autor, ano, sta) 
+  mycursor.execute(sql, val)
+
+  mydb.commit()
+
+  print(mycursor.rowcount, "Inserido com Sucesso.")
+  mycursor.close()
